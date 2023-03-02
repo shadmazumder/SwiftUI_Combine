81148fd885d311c6779ed97e11392dc24e6d3e10
shadmazumder <shadmazumder@gmail.com>
2023-03-02 16:44:09 +0600

    Initial Commit

---
+++ SwiftUI_combine/SwiftUI_combineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_combineApp.swift
+//  SwiftUI_combine
+//
+//  Created by Shad Mazumder on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_combineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
